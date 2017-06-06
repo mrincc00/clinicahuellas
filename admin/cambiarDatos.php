8ed2d023127496af52d08ff3983b4570f1f55251
Alba Ortega <albaortegaflores95@gmail.com>
2017-06-06 13:10:24 +0200

    Modificar datos de la clínica

---
+++ admin/cambiarDatos.php
@@ -0,0 +1,171 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Clínica Huellas</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+                <script language="javascript" src="../assets/js/admin.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	</head>
+	<body id="bodyOfertas">
+
+		<!-- MENU -->
+			<header id="header" style="padding-top:0;">
+				<div class="inner" >
+					<p class="logo" style="font-size:150%"><strong>Clínica Huellas</strong> </p><br/>
+					<p class="logo" id="tipo"><strong>ADMINISTRADOR</strong></p>
+					<a id="cerrar" href="../cerrarSesion.php"><img style="width:30%" src="../images/logout.png"></a>
+                                        <nav id="menuNormal" style="padding-top:2%; width:90%;">
+						<a href="adminDatos.php">Datos</a>
+						<a href="registrar.php">Registrar y Editar</a>
+                                                <a href="listaClientes.php">Clientes</a>
+                                                <a href="copiaBd.php">Copiar BD</a>
+                                                <a href="eliminarComentarios.php">Eliminar comentarios</a>
+                                                <a ><strong>Datos clínica</strong></a>
+                                                <a href="cambiarOfertas.php">Modificar ofertas</a>
+                                                <a href="insertarFotos.php">Actualizar galeria</a>
+					</nav>
+<ul id="menu" style="color:black;list-style: none; float:left; margin-left:0; width:70%; margin-top:2%;" >
+                                            <li id="etiquetaMenu" onclick="return abrirMenu(0);" style="padding:0; margin:0; float:left"><a href ><img src="../images/menu.png" style="width:70%; margin-bottom:3%; margin-left:0;"></a></li>
+                                            <ul id="desplegable" style="display:none; list-style: none; margin-top:15% " >
+                                                <li class="itemMenu"><a href="adminDatos.php" style="width:100%;">Datos</a></li>
+						<li class="itemMenu"><a href="registrar.php" style="width:100%;">Registrar y Editar</a></li>
+                                                <li class="itemMenu"><a style="width:100%;" href="listaClientes.php">Clientes</a></li>
+                                                <li class="itemMenu"><a style="width:100%;" href="copiaBd.php">Copiar BD</a></li>
+                                                <li class="itemMenu"><a style="width:100%;" href="eliminarComentarios.php">Eliminar comentarios</a></li>
+                                                <li class="itemMenu"><a style="width:100%; color:white;">Datos clínica</a></li>
+                                                <li class="itemMenu"><a style="width:100%;" href="cambiarOfertas.php">Modificar ofertas</a></li>
+                                                <li class="itemMenu"><a style="width:100%;" href="insertarFotos.php">Actualizar galeria</a></li>
+</ul>
+</ul>
+
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section class="cabecera" id="banner" style="background-image:url(../images/fondopeque.jpeg);">
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center" style="padding:2% 0 0 0">
+				<div class="inner">
+					<form id="formEmail" method="post" action="" onSubmit="return completoEmail();" >
+                                           <p>Si ha cambiado el <strong>EMAIL</strong> de la clínica, puede modificarlo aquí:</p>
+                                           
+                                           <?php
+include '../connect.php';
+if (!($sentencia = $conn->prepare("SELECT correo FROM Datos WHERE 1"))) {
+          echo "Fallo en la preparación: (" . $conn->errno . ") " . $conn->error;
+        }
+
+        if (!$sentencia->execute()) {
+          echo "Fallo la ejecución: (" . $sentencia->errno . ") " . $sentencia->error;
+        }
+        if (!($result = $sentencia->get_result())) {
+		      echo "No se ha podido establecer la conexion con el servidor";
+	}
+	if ($result->num_rows > 0) {
+             $nombres = array();
+
+	     while($row = $result->fetch_array()){
+		    array_push($nombres,$row[0]);
+                    $email=$row[0];
+                    echo"<input type='text' id='email' name='email' placeholder='E-mail nuevo' value='$email'/><br/>";
+            }
+        } 
+?>
+
+                                           <input type="submit" value="cambiar email" name="tipoSubmit"/>								
+					</form>					
+				</div>
+<div class="inner">
+					<form id="formDescripcion" method="post" action="" onSubmit="return completoDesc();" >
+                                           <p>Si ha cambiado la <strong>DESCRIPCIÓN</strong> de la clínica, puede modificarlo aquí:</p>
+                                           
+<?php
+include '../connect.php';
+if (!($sentencia = $conn->prepare("SELECT descripcion FROM Datos WHERE 1"))) {
+          echo "Fallo en la preparación: (" . $conn->errno . ") " . $conn->error;
+        }
+
+        if (!$sentencia->execute()) {
+          echo "Fallo la ejecución: (" . $sentencia->errno . ") " . $sentencia->error;
+        }
+        if (!($result = $sentencia->get_result())) {
+		      echo "No se ha podido establecer la conexion con el servidor";
+	}
+	if ($result->num_rows > 0) {
+             $nombres = array();
+
+	     while($row = $result->fetch_array()){
+		    array_push($nombres,$row[0]);
+                    $descripcion=$row[0];
+                    echo"<textarea type='text' id='descripcion' name='descripcion'  rows='6'>$descripcion</textarea><br/>";
+            }
+        } 
+?>
+                                           <input type="submit" value="cambiar descripción" name="tipoSubmit" />
+                                           <button type="button" onclick="recargar();">Cancelar</button>								
+					</form>					
+				</div>
+			</section>
+
+	</body>
+        <script type="text/javascript">
+                 function completoEmail(){ 
+                     var email=document.getElementById("email").value;
+                     if(email.length < 10){ alert("Introduzca un e-mail válido"); return false;}
+                     return true;
+                 }
+                 function completoDesc(){ 
+                     var descripcion=document.getElementById("descripcion").value;
+                     if(descripcion.length == 0){ alert("Introduzca una descripción"); return false;}
+                     return true;
+                 }
+	</script>    
+<?php
+session_start();
+include_once '../connect.php';
+if($_POST!=null){
+$tipo=$_POST['tipoSubmit'];
+$atributo=null;
+$dato=null;
+if($tipo=="cambiar descripción"){ $atributo='descripcion'; $dato=$_POST['descripcion'];}
+if($tipo=="cambiar email"){ $atributo='correo'; $dato=$_POST['email'];}
+if (!($sentencia = $conn->prepare("UPDATE `Datos` SET $atributo='$dato' WHERE 1"))) {
+          echo "Fallo en la preparación: (" . $conn->errno . ") " . $conn->error;
+ }
+ if (!$sentencia->execute()) {
+       echo "Fallo en la ejecucion";
+ }else{
+      echo "<script>window.onload=function(){location.replace('cambiarDatos.php?ok=1');}</script>";
+	 
+ }
+}
+if($_GET!=null){
+    echo "<script>window.onload=function(){alert('Datos cambiados');}</script>";
+}
+?>  
+<?php
+if($_SESSION==null)  echo "<script>document.getElementsByTagName('body')[0].onload=function(){setTimeout(\"location.href='../index.php'\", 1);}</script>";
+?> 
+<script>
+function abrirMenu(valor){
+if(valor==0){
+document.getElementById("desplegable").style.display='block';
+document.getElementById("etiquetaMenu").onclick="return abrirMenu(1)";
+}
+if(valor==1){
+document.getElementById("desplegable").style.display='none';
+document.getElementById("etiquetaMenu").onclick="return abrirMenu(0)";
+}
+return false;
+}
+function recargar(){
+$("#bodyOfertas").load("#bodyOfertas");
+}
+</script> 
+
+</html>
